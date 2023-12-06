980fab0925231eb43caa7e7c01a5b418e96ff2ad
manalipatel9 <136845837+manalipatel9@users.noreply.github.com>
2023-12-06 17:07:42 -0600

    Update api_keys.py

--- module 6 challenge/starter_code/api_keys.py
+++ module 6 challenge/starter_code/api_keys.py
@@ -1,6 +1,6 @@
 # OpenWeatherMap API Key
-weather_api_key = "444a3b6fa5cf6037715ab92b424fd1d6"
+weather_api_key = " "
 #before turning, take out API key
 
 # Geoapify API Key
-geoapify_key = "b1b97dcf1ce648e9b6d974f38b8afc2e"
+geoapify_key = " "
