a84f3d8c2fb92bd2888f225ba112cb00e51396a4
manalipatel9 <136845837+manalipatel9@users.noreply.github.com>
2023-12-06 17:04:14 -0600

    Add files via upload

---
+++ module 6 challenge/starter_code/api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "444a3b6fa5cf6037715ab92b424fd1d6"
+#before turning, take out API key
+
+# Geoapify API Key
+geoapify_key = "b1b97dcf1ce648e9b6d974f38b8afc2e"
